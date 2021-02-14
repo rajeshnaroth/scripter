d4ebc033518ada490a0e35c19ed699f7ffae4825
Raj Naroth <rajnaroth@Rajeshs-MacBook-Pro.local>
2021-02-13 20:22:31 -0800

    Initial checkin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # scripter
+
+A nifty utiliy to translate a major/minor scale to one of the 72 Full Carnatic scales aka Melakartha ragas
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { HandleMIDI, ParameterChanged, ProcessMIDI, Idle } from "./ragaTranslator";
+
+// Remove these lines once you copy the built files from dist
+HandleMIDI();
+ParameterChanged();
+ProcessMIDI();
+Idle();
